4b560ca2c90761502a2572a8510f654876d6ea6b
Kashif <ahamed.kashif@northsouth.edu>
2021-03-22 12:26:10 +0600

    car show unresolved

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,14 @@ $(".jumbotron").css({ height: $(window).height() + "px" });
 $(window).on("resize", function() {
     $(".jumbotron").css({ height: $(window).height() + "px" });
 });
+$(document).ready(function() {
+
+    $('.color-choose input').on('click', function() {
+        var headphonesColor = $(this).attr('data-image');
+
+        $('.active').removeClass('active');
+        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+
+});
